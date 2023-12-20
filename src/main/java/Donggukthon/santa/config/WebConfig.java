923c6aefb989f629334f2e1e076b118a9eade9be
유수민 <proysm5154@naver.com>
2023-12-20 11:01:39 +0900

    Update: CORS

--- src/main/java/Donggukthon/santa/config/WebConfig.java
+++ src/main/java/Donggukthon/santa/config/WebConfig.java
@@ -1,8 +1,10 @@
 package Donggukthon.santa.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
