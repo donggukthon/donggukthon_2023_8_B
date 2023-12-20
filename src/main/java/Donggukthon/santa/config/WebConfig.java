0e147e9ebbf469579154933ee3700b08c9896854
유수민 <proysm5154@naver.com>
2023-12-20 10:31:37 +0900

    Update: CORS

--- src/main/java/Donggukthon/santa/config/WebConfig.java
+++ src/main/java/Donggukthon/santa/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://my-3d-tree.vercel.app", "https://www.my-3d-tree.com") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
-                .allowCredentials(false) // 쿠키 인증 요청 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 
