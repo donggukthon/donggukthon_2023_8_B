47fccb0a1aa8581498d5e9d2c82ced43d6339cc3
유수민 <proysm5154@naver.com>
2023-12-20 11:10:34 +0900

    Fix: ErrorStatus status

--- src/main/java/Donggukthon/santa/web/apiResponse/ErrorStatus.java
+++ src/main/java/Donggukthon/santa/web/apiResponse/ErrorStatus.java
@@ -8,18 +8,18 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorStatus {
 
     /** /Token **/
-    INVALID_TOKEN(400, "FAIL", "유효하지 않은 토큰 값입니다."),
+    INVALID_TOKEN(400, "FAILED", "유효하지 않은 토큰 값입니다."),
 
     /** /main **/
-    READ_TREE_DECORATION(400, "FAIL", "트리의 데코레이션들 조회를 실패했습니다."),
+    READ_TREE_DECORATION(400, "FAILED", "트리의 데코레이션들 조회를 실패했습니다."),
 
     /** /user **/
-    HAVE_USER(500, "FAIL", "이미 존재하는 이매일입니다."),
-    JOIN_USER(500, "FAIL", "회원가입에 실패하였습니다."),
-    LOGIN_USER(401, "FAIL", "비밀번호가 일치하지 않습니다."),
+    HAVE_USER(500, "FAILED", "이미 존재하는 이매일입니다."),
+    JOIN_USER(500, "FAILED", "회원가입에 실패하였습니다."),
+    LOGIN_USER(401, "FAILED", "비밀번호가 일치하지 않습니다."),
 
     /** /certification **/
-    NON_DONATE(400, "FAIL", "아직 기부하지 않은 멤버입니다.");
+    NON_DONATE(400, "FAILED", "아직 기부하지 않은 멤버입니다.");
 
     /** /submission **/
 
